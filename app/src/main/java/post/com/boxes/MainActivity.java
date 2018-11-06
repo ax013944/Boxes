da5979c8b9e951f329c11fe28d95c520cfe84158
aax013944 <aax013944@gmail.com>
2018-11-06 15:50:19 +0800

    mid

--- app/src/main/java/post/com/boxes/MainActivity.java
+++ app/src/main/java/post/com/boxes/MainActivity.java
@@ -1,13 +1,41 @@
 package post.com.boxes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void box(View view) {
+        EditText Height = findViewById(R.id.ed_1);
+        EditText Width = findViewById(R.id.ed_2);
+        EditText Length = findViewById(R.id.ed_3);
+        String h = Height.getText().toString();
+        String w = Width.getText().toString();
+        String l = Length.getText().toString();
+        float height = Float.parseFloat(h);
+        float width = Float.parseFloat(h);
+        float length = Float.parseFloat(h);
+
+        if (height <= 23 && width <= 14 && length <= 13) {
+            float check = 3;
+            Intent intent = new Intent(this, ResultActivity.class);
+            startActivity(intent);
+        } else {
+            float check = 5;
+            Intent intent = new Intent(this, ResultActivity2.class);
+            startActivity(intent);
+        }
+
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
